01322859267f5c86bd6eae0bb5813b3ca9aacb94
Derek Anderson <backupacct4664@gmail.com>
2023-01-16 11:50:04 -0600

    Challenge 9: Add Policyholders happy path to cypress tests.

--- cypress/integration/happy_path.spec.ts
+++ cypress/integration/happy_path.spec.ts
@@ -1,17 +1,27 @@
 /// <reference types="cypress" />
 
 describe('happy path', () => {
+
   it('runs happy path successfully', () => {
     cy.visit('/');
     cy.getTestEl('table_link').should('be.visible');
     cy.getTestEl('you_go_link').should('be.visible');
     cy.getTestEl('policyholders_link').should('be.visible');
 
-    /**
-     * TODO: Challenge 10 - Update this test
-     * - Click the Policyholders sidebar link
-     * - Assert that a network request is made
-     * - Assert that data from the network is displayed
-     */
+    cy.intercept({
+        method: 'GET',
+        url: '/api/policyholders',
+    },{fixture: 'policyHolders.json'}
+    ).as('getPolicyHolders')
+    
+    cy.get('[data-testid="policyholders_link"]').click()
+     
+    cy.wait('@getPolicyHolders', {timeout: 3000}).then((interception) => {
+        expect(interception.response.statusCode).equal(200)
+    })
+     // Cell for 'Age' row value should be visible
+    cy.get(':nth-child(2) > .css-1miykz9-MuiTableCell-root').should('be.visible')
+    // The value in the 'Age' row value cell should match the value in our text fixture, verifying that the data displayed is from the intercepted request
+    cy.get(':nth-child(2) > .css-1miykz9-MuiTableCell-root').contains(55)
   });
 });
